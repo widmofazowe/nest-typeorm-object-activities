061d741af35a96db67ca3f39b1c4a7c54d5f27fe
widmo <widomski.pawel@gmail.com>
2022-09-13 14:16:42 +0200

    update endpoint

--- src/activity.controller.ts
+++ src/activity.controller.ts
@@ -3,7 +3,7 @@ import { ActivityService } from './activity.service';
 import { AddActivityDto } from './add-activity.dto';
 
 export const getActivityController = (baseUrl: string): any => {
-  @Controller(`/activity/${baseUrl}/comment`)
+  @Controller(`/activity/${baseUrl}/activity`)
   class ActivityController {
     constructor(private activityService: ActivityService) {}
 
